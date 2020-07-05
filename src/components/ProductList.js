64163d026ab86c2a6fa26e2afcb3ec513649e084
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-07-05 18:45:06 +0430

    product list & bascket grid ui ok

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,20 +1,63 @@
 import React from "react";
 import ProductCard from "./ProductCard";
 import ShoppingBasket from "./ShoppingBasket";
-import { Container } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 
 export default function ProductList() {
   return (
-    <div className="page">
-      <Grid container spacing={1}>
-        <Grid item xs={6} sm={3}>
-          <ProductCard />
-        </Grid>
-        <Grid item xs={6} sm={3}>
-          <ProductCard />
+    <Grid container spacing={1}>
+      <Grid item xs={4} sm={8}>
+        <Grid container spacing={1}>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
+          <Grid item xs={4} sm={4}>
+            <ProductCard />
+          </Grid>
         </Grid>
       </Grid>
-    </div>
+      <Grid item sm={4}>
+        <ShoppingBasket />
+      </Grid>
+    </Grid>
   );
 }
--- src/components/ShoppingBasket.js
+++ src/components/ShoppingBasket.js
@@ -1,81 +1,69 @@
 import React from "react";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import Grid from "@material-ui/core/Grid";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
-import PlayArrowIcon from "@material-ui/icons/PlayArrow";
-import SkipNextIcon from "@material-ui/icons/SkipNext";
-import AddIcon from "@material-ui/icons/Add";
-import { Container } from "@material-ui/core";
 import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
-import CardActionArea from "@material-ui/core/CardActionArea";
-const useStyles = makeStyles((theme) => ({
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
   root: {
-    display: "flex",
+    marginTop: 10,
+    minWidth: 275,
   },
-  sidebar: {
-    maxWidth: 345,
-  },
-  details: {
-    display: "flex",
-    flexDirection: "column",
-  },
-  content: {
-    flex: "1 0 auto",
-  },
-  cover: {
-    width: 151,
-  },
-  controls: {
-    display: "flex",
-    alignItems: "center",
-    paddingLeft: theme.spacing(1),
-    paddingBottom: theme.spacing(1),
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   typography: {
     fontFamily: "Almarai",
     fontSize: "1rem",
+    fontWeight: "bold",
   },
-  addIcon: {
-    height: 38,
-    width: 38,
+  title: {
+    fontSize: 14,
   },
-  media: {
-    height: 140,
+  pos: {
+    marginBottom: 12,
   },
-}));
-export default function ShoppingBasket() {
+});
+
+export default function OutlinedCard() {
   const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
 
   return (
-    <Card className={classes.sidebar}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+    <Card className={classes.root} variant="outlined">
+      <CardContent>
+        <Typography
+          className={(classes.title, classes.typography)}
+          color="textSecondary"
+          gutterBottom
+        >
+          سبد خرید
+        </Typography>
+        <Typography className={classes.typography} variant="h5" component="h2">
+          سبد خرید شما
+        </Typography>
+        <Typography
+          className={(classes.pos, classes.typography)}
+          color="textSecondary"
+        >
+          خالی می باشد
+        </Typography>
+        <Typography
+          className={classes.typography}
+          variant="body2"
+          component="p"
+        >
+          قابل پرداخت <br />
+          {"صفر ریال"}
+        </Typography>
+      </CardContent>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
+        <Button className={classes.typography} size="small">
+          پرداخت
         </Button>
       </CardActions>
     </Card>
