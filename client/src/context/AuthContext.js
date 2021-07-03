34fc6b41176c8a389d03be1749804299992a07b8
Shahr Zad <gh.shahrzad@gmail.com>
2021-07-03 13:59:18 +0430

    context update

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,22 +1,34 @@
 //import axios from 'axios';
 import React, {useEffect, useState, createContext} from 'react'
 import axios from 'axios'
+
 const AuthContext = createContext();
 
 function AuthContextProvider(props) {
   const [isLoggedIn, setisLoggedIn] = useState(undefined);
-  
+  const [user, setUser] = useState(null);
+
   async function getLoggedIn(){
-    const loggedInRes = true//await axios.get('/shipping');
-    setisLoggedIn(loggedInRes);
+    try{
+      const loggedInRes = await axios.get('/auth');
+      setisLoggedIn(loggedInRes.data);
+  
+      const roleRes = await axios.get('/role')
+      setUser(roleRes.data)
+    }
+    catch(err){console.log(err)}
+
   }
   
   useEffect(() => {
       getLoggedIn();
   }, []);
 
+  console.log("USER Role",user)
+  console.log("LoggedIn",isLoggedIn)
+
   return (
-      <AuthContext.Provider value={ {isLoggedIn, getLoggedIn}}>
+      <AuthContext.Provider value={ {isLoggedIn, getLoggedIn, user}}>
         {props.children}
       </AuthContext.Provider>
   )
