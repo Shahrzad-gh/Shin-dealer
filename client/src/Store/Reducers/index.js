6316be1dcabc30fb70ef7a097880006a6af0617e
Shahr Zad <gh.shahrzad@gmail.com>
2021-07-18 17:21:43 +0430

    redux

--- client/src/Store/Reducers/index.js
+++ client/src/Store/Reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import productReducer from './productReducer';
 import cartReducer from './cartReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
     product: productReducer,
     cart: cartReducer,
+    user : userReducer
 
 });
 
