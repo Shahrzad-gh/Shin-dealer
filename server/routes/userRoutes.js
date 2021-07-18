c30953d739f1fe0d82ddbdc3785effff6dd12202
Shahr Zad <gh.shahrzad@gmail.com>
2021-07-18 17:20:56 +0430

    getuserId - /:id

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const userController = require('../controllers/userController')
 
 const router = Router();
-router.get('/getUserById', userController.getUserById_get);
+router.get('/getUserById/:id', userController.getUserById_get);
 router.get('/getallusers', userController.getAllUsers_get);
 
 module.exports = router;
