c28bc027a6bba800acbc4a6f8433a02abfaae0d9
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-07 15:51:40 +0430

    update

--- server/routes/razorPayRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { requireAuth, checkUser, adminMiddleware } = require('../middlewares/commonMiddleware');
-
-const router = Router();
-
-
-
-
-module.exports = router;
