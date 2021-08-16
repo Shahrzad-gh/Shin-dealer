ae32330f76bd696ea8e02410cc979864dff2a039
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-16 17:53:36 +0430

    add user profile

--- client/src/components/User/Profile.js
+++ client/src/components/User/Profile.js
@@ -1,8 +1,38 @@
-import React from 'react'
+import { CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
+import React from 'react';
+import { Card} from '@material-ui/core';
 
+const useStyles = makeStyles({
+  root: {
+    padding: '20px',
+    display: 'flex'
+  },
+  card: {
+    border: '1px solid blue'
+  },
+  typography : {
+    fontFamily: "Almarai", fontSize: "1rem",
+    padding: 12,
+    margin: 0
+  }
+})
 function Profile() {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardMedia>
+          <img src="" alt="" title=""/>
+        </CardMedia>
+        <CardContent>
+          <Typography className={classes.typography}>
+            <p>userName</p>
+          </Typography>
+        </CardContent>
+      </Card>
+      <Card className={classes.card}>
+        
+      </Card>
     </div>
   )
 }
