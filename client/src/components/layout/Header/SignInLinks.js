b8965f951f75ccba4a3630a6e12bfdaa29bf8a7d
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-18 17:21:17 +0430

    test

--- client/src/components/layout/Header/SignInLinks.js
+++ client/src/components/layout/Header/SignInLinks.js
@@ -74,60 +74,14 @@ function SignInLinks(props) {
   }
 
   const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <Link to="profile"><MenuItem onClick={handleMenuClose}>Profile</MenuItem></Link>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-      <MenuItem onClick={handleSignOut}>SignOut</MenuItem>
-    </Menu>
-  );
 
   const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label="show 11 new shoppingbasket" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <ShoppingBasketIcon />
-          </Badge>
-        </IconButton>
-        <p>Shopping</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <Link to="Profile"><p>Profile</p></Link>        
-      </MenuItem>
-    </Menu>
-  );
-
 
     return (
       <authContext.Consumer>
         {(user) => ( 
         <div>
+          {console.log(user)}
          <div className={classes.sectionDesktop}>
             <IconButton aria-label="show 17 new shoppingbasket" color="inherit">
               <Badge badgeContent={0} color="secondary">
@@ -158,9 +112,51 @@ function SignInLinks(props) {
               <MoreIcon />
             </IconButton>
           </div>
-        {renderMobileMenu}
-        {renderMenu}
-        </div>)}      
+          
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 11 new shoppingbasket" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <ShoppingBasketIcon />
+          </Badge>
+        </IconButton>
+        <p>Shopping</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <Link to={{pathname:"Profile", state: user}}><p>Profile</p></Link>        
+      </MenuItem>
+    </Menu>
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <Link to={{pathname:"profile", state: user}}><MenuItem onClick={handleMenuClose}>Profile</MenuItem></Link>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      <MenuItem onClick={handleSignOut}>SignOut</MenuItem>
+    </Menu>
+        </div>
+        )}      
 </authContext.Consumer>
     )}
 
