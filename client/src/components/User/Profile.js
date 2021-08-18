7797ecf48d96f9fff0cf7a3fda8927486e2dc7ad
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-18 17:20:56 +0430

    update

--- client/src/components/User/Profile.js
+++ client/src/components/User/Profile.js
@@ -1,7 +1,9 @@
 import { CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Card} from '@material-ui/core';
 import Box from '@material-ui/core/Box';
+import { getUserById } from '../../Store/actions/userAction';
+import { useDispatch, useSelector } from 'react-redux';;
 
 const useStyles = makeStyles({
   root: {
@@ -16,10 +18,27 @@ const useStyles = makeStyles({
     fontFamily: "Almarai", fontSize: "1rem",
     padding: 12,
     margin: 0
+  },
+  status:{
+    display: 'flex',
+    justifyContent: 'flex-end'
   }
 })
-function Profile() {
+function Profile(props) {
   const classes = useStyles();
+  const dispatch = useDispatch()
+  const userId = "60d98604214b94312084c0b1"
+  const user = useSelector(state => state.user)
+  const userInfo = user.user[userId]
+  useEffect(() => {
+    const payload = {
+      params : {
+        userId
+      }
+    }
+    dispatch(getUserById(payload))
+  }, [])
+  console.log(userInfo)
   return (
     <Box className={classes.root}>
       <Box display="flex" p={1} height="100%" width="30%">
@@ -27,11 +46,15 @@ function Profile() {
       </Box>
       <Box>
       <Box display="flex" p={1} flexGrow={1} height="40%">
-      {"userInfo"}
       <div>
-        <div className={classes.header}></div>
+        <div className={classes.header}>{"userInfo"}</div>
         <div className={classes.status}>
-          {"status.map(item => <div> {item} </div>"}
+         {userInfo && (
+         <>
+         <div>{userInfo.firstName}</div>
+         <div>{userInfo.lastName}</div>
+         </>
+         )}
         </div>
       </div>
       </Box>
