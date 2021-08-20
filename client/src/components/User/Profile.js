6a086c03a84a4522fecdb8da620a8118ba3d5351
Shahr Zad <gh.shahrzad@gmail.com>
2021-08-20 12:27:27 +0430

    user profile - show user orders

--- client/src/components/User/Profile.js
+++ client/src/components/User/Profile.js
@@ -3,7 +3,17 @@ import React, {useEffect} from 'react';
 import { Card} from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 import { getUserById } from '../../Store/actions/userAction';
-import { useDispatch, useSelector } from 'react-redux';;
+import { useDispatch, useSelector } from 'react-redux';
+import {getUserOrdersByUserId} from "../../Store/actions/orderActions"
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Title from '../Admin/Title';
+import moment from 'moment';
+import { Link } from "react-router-dom";
+import { Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -21,6 +31,11 @@ const useStyles = makeStyles({
   },
   status:{
     display: 'flex',
+  },
+  profile: {
+    margin: '5px',
+    border: '1px solid gray',
+    borderRadius: '10px'
   }
 })
 function Profile(props) {
@@ -30,6 +45,8 @@ function Profile(props) {
   const user = useSelector(state => state.user)
   const userId = props.location.query.id;
   const userInfo = user.user[userId]
+  const orders = useSelector((state) => state.orders)
+  var status = "";
 
   useEffect(() => {
     const payload = {
@@ -37,18 +54,20 @@ function Profile(props) {
         userId
       }
     }
-    dispatch(getUserById(payload))
+    dispatch(getUserById(payload));
+    dispatch(getUserOrdersByUserId(payload))
   }, [])
 
   console.log(userInfo)
+  console.log(orders.orders.orders)
 
   return (
     <Box className={classes.root}>
-      <Box display="flex" p={1} height="100%" width="30%">
+      <Box className={classes.profile} display="flex" p={1} height="100%" width="30%">
       {"user profile"}
       </Box>
       <Box width="70%">
-      <Box display="flex" p={1} flexGrow={1} height="40%">
+      <Box className={classes.profile} display="flex" p={1} flexGrow={1} height="40%">
       <div>
         <div className={classes.header}>{"userInfo"}</div>
         <div className={classes.status}>
@@ -62,8 +81,39 @@ function Profile(props) {
         </div>
       </div>
       </Box>
-      <Box display="flex" p={1} flexGrow={1} height="58%">
-      {"Order List"}
+      <Box className={classes.profile} display="flex-block" p={1} flexGrow={1} height="58%">
+      <Title>All Orders</Title>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            {/* <TableCell>Name</TableCell>
+            <TableCell>Ship To</TableCell> */}
+            <TableCell>OrderId</TableCell>
+            <TableCell>Payment Status</TableCell>
+            <TableCell>Sale Amount</TableCell>
+            <TableCell>Manage</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+        {orders.orders.orders && orders.orders.orders.map((row) =>
+          <TableRow key={row._id}>
+            <TableCell className={classes.typography}>{moment().calendar(row.createdAt)}</TableCell>
+            {/* <TableCell><UserInfo key ={index} data={row.user} /></TableCell>
+            <TableCell className={classes.typography}>{row.shipTo}</TableCell> */}
+            <TableCell className={classes.typography}>{row._id}</TableCell>  
+            <TableCell >{row.paymentStatus === 'complete' ? <Typography className={classes.paymentDone}>{row.paymentStatus}</Typography>
+             : <Typography className={classes.paymentPending}>{row.paymentStatus}</Typography>}</TableCell>
+            <TableCell className={classes.typography} align="right">${row.totalAmount}</TableCell>
+            <TableCell className={classes.typography}>
+              <Link to={{pathname:`ManageOrder/${row._id}`, param:{status}}}>
+                <Button variant="contained" color="primary" >Manage</Button>
+                </Link>
+                </TableCell>
+          </TableRow>
+        )}
+        </TableBody>
+      </Table>
       </Box>
       </Box>
     </Box>
