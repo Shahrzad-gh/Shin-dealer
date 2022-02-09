69a90832fe8de18365a40416a66ff15ea1be5c4e
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-09 16:01:55 +0330

    update

--- client/src/components/Admin/AllOrders.js
+++ client/src/components/Admin/AllOrders.js
@@ -1,40 +1,39 @@
-import React from 'react'
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Title from './Title';
-import moment from 'moment';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Title from "./Title";
+import moment from "moment";
+import { makeStyles } from "@material-ui/core/styles";
 //import UserInfo from './UserInfo';
-import { Button, Typography } from '@material-ui/core';
+import { Button, Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import DoneIcon from '@material-ui/icons/Done';
-import ClearIcon from '@material-ui/icons/Clear';
+import DoneIcon from "@material-ui/icons/Done";
+import ClearIcon from "@material-ui/icons/Clear";
 const useStyles = makeStyles(() => ({
   paymentPending: {
     fontFamily: "Almarai",
-    color: 'blue',
-    fontWeight: 'bold'
+    color: "blue",
+    fontWeight: "bold",
   },
   paymentDone: {
     fontFamily: "Almarai",
-    color: 'green',
-    fontWeight: 'bold'
+    color: "green",
+    fontWeight: "bold",
   },
   typography: {
     fontFamily: "Almarai",
     fontSize: "1rem",
-    fontWeight: 'normal'
+    fontWeight: "normal",
   },
 }));
 
 function AllOrders(props) {
-  console.log(props)
   const classes = useStyles();
-  const orders = props.location.query && props.location.query.orders
-  console.log(orders);
+  const orders = props.location.query && props.location.query.orders;
+
   var status = "";
   return (
     <React.Fragment>
@@ -54,30 +53,57 @@ function AllOrders(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-         {orders && orders.map((row, index) => (
-          <TableRow key={index}>
-            <TableCell className={classes.typography}>{moment().calendar(row.createdAt)}</TableCell>
-            {/* <TableCell><UserInfo key ={index} data={row.user} /></TableCell>
-            <TableCell className={classes.typography}>{row.shipTo}</TableCell> */}
-            <TableCell className={classes.typography}>{row._id}</TableCell> 
-            <TableCell className={classes.typography}>{row.orderStatus.map((item) => 
-            item.isCompleted ? status = item.type : status = "Ordered"
-            )}</TableCell> 
-            <TableCell className={classes.typography}>{row.paymentType}</TableCell>
-            <TableCell >{row.paymentStatus === 'complete' ? <Typography className={classes.paymentDone}>{row.paymentStatus}</Typography>
-             : <Typography className={classes.paymentPending}>{row.paymentStatus}</Typography>}</TableCell>
-            <TableCell className={classes.typography} align="right">${row.totalAmount}</TableCell>
-            <TableCell className={classes.typography}>
-              <Link to={{pathname:`ManageOrder/${row._id}`, param:{status}}}>
-                <Button variant="contained" color="primary" >Manage</Button>
-                </Link>
+          {orders &&
+            orders.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell className={classes.typography}>
+                  {moment().calendar(row.createdAt)}
                 </TableCell>
-          </TableRow>
-        ))}
+                {/* <TableCell><UserInfo key ={index} data={row.user} /></TableCell>
+            <TableCell className={classes.typography}>{row.shipTo}</TableCell> */}
+                <TableCell className={classes.typography}>{row._id}</TableCell>
+                <TableCell className={classes.typography}>
+                  {row.orderStatus.map((item) =>
+                    item.isCompleted
+                      ? (status = item.type)
+                      : (status = "ordered")
+                  )}
+                </TableCell>
+                <TableCell className={classes.typography}>
+                  {row.paymentType}
+                </TableCell>
+                <TableCell>
+                  {row.paymentStatus === "complete" ? (
+                    <Typography className={classes.paymentDone}>
+                      {row.paymentStatus}
+                    </Typography>
+                  ) : (
+                    <Typography className={classes.paymentPending}>
+                      {row.paymentStatus}
+                    </Typography>
+                  )}
+                </TableCell>
+                <TableCell className={classes.typography} align="right">
+                  ${row.totalAmount}
+                </TableCell>
+                <TableCell className={classes.typography}>
+                  <Link
+                    to={{
+                      pathname: `ManageOrder/${row._id}`,
+                      param: { status },
+                    }}
+                  >
+                    <Button variant="contained" color="primary">
+                      Manage
+                    </Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </React.Fragment>
   );
 }
 
-export default AllOrders
+export default AllOrders;
