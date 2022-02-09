3eafaafc65a7e46862b91cf4823df8cd5218b3e9
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-09 15:59:06 +0330

    update

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const productRoutes = require('./routes/productRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const cartRoutes = require('./routes/cartRoutes');
-const cookieParser = require('cookie-parser');
-const { requireAuth, checkUser, adminMiddleware } = require('./middlewares/commonMiddleware');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const cookieParser = require("cookie-parser");
+const {
+  requireAuth,
+  checkUser,
+  adminMiddleware,
+} = require("./middlewares/commonMiddleware");
+const dotenv = require("dotenv");
+const cors = require("cors");
 //const path = require("path");
-const userRoutes = require('./routes/userRoutes');
-const orderRoutes = require('./routes/orderRoutes')
-const paymentRoutes = require('./routes/paymentRoutes');
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const paymentRoutes = require("./routes/paymentRoutes");
 //const crypto = require('crypto');
 
 dotenv.config();
@@ -19,31 +23,42 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  preflightContinue : false,
-  credentials: true,
-  
-}))
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    preflightContinue: false,
+    credentials: true,
+  })
+);
 
 //view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // database connection
-mongoose.connect(process.env.MDB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
-  .then((result) => app.listen(PORT, console.log(`app connected to MongoDB & listen to port: ${PORT}`)))
+mongoose
+  .connect(process.env.MDB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) =>
+    app.listen(
+      PORT,
+      console.log(`app connected to MongoDB & listen to port: ${PORT}`)
+    )
+  )
   .catch((err) => console.log(err));
 
 // routes
 //app.get('*');
 // app.get('/shipping', requireAuth);
 // app.get('/admin', requireAuth, checkUser, adminMiddleware);
-app.post('/verification' , (req, res) => {
-  res.json({status: 'OK' })
-})
+app.post("/verification", (req, res) => {
+  res.json({ status: "OK" });
+});
 
 app.use(authRoutes);
 app.use(productRoutes);
