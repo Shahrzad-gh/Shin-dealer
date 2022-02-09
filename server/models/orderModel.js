fa32fcea0407f21f925ad1b9e99eee1933e2acbe
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-09 15:59:45 +0330

    update order model - order status add finished

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -1,49 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const orderSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  totalAmount: {
-    type: Number,
-    required: true
-  },
-  items: [
-    {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product'},
-      count: { type: Number, required: true},
-      payable: { type: Number, required: true}
-    }
-  ],
-  paymentStatus: {
-    type: String,
-    enum: ['pending', 'completed', 'cancelled', 'refund'],
-    required: true,
-  },
-  paymentType: {
-    type: String,
-    enum: ['card', 'cod'],
-    required: true
-  },
-  orderStatus: [
-    {
-      type:{ 
-        type:String,
-        enum: ['ordered', 'packed', 'shipped', 'delivered'],
-        default: 'ordered'
+const orderSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    items: [
+      {
+        product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        count: { type: Number, required: true },
+        payable: { type: Number, required: true },
       },
-      date: {
-        type: Date
+    ],
+    paymentStatus: {
+      type: String,
+      enum: ["pending", "completed", "cancelled", "refund"],
+      required: true,
+    },
+    paymentType: {
+      type: String,
+      enum: ["card", "cod"],
+      required: true,
+    },
+    orderStatus: [
+      {
+        type: {
+          type: String,
+          enum: ["ordered", "packed", "shipped", "delivered", "finished"],
+          default: "ordered",
+        },
+        date: {
+          type: Date,
+        },
+        isCompleted: {
+          type: Boolean,
+          default: false,
+        },
       },
-      isCompleted:{
-        type: Boolean,
-        default: false
-      }
-
-    }
-  ]
-}, { timestamps: true });
+    ],
+  },
+  { timestamps: true }
+);
 
 //mongoose.set('useFindAndModify', false);
 
-const Order = mongoose.model('order', orderSchema);
+const Order = mongoose.model("order", orderSchema);
 
 module.exports = Order;
