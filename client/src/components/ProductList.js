cc2d9f4d12d3741414dea49458e43da7498a8045
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-09 16:01:34 +0330

    remove basket

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -3,101 +3,98 @@ import { makeStyles } from "@material-ui/core/styles";
 import ProductCard from "./ProductCard";
 import Grid from "@material-ui/core/Grid";
 import Basket from "./Basket";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import { useSelector, useDispatch } from 'react-redux'
-import { getProductsByCategory} from "../Store/actions/productActions";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductsByCategory } from "../Store/actions/productActions";
 
 const useStyles = makeStyles({
   listStyle: {
-    listStyle: 'none',
+    listStyle: "none",
   },
   displayList: {
-    display: '-webkit-inline-box'
+    display: "-webkit-inline-box",
   },
-  Nopadding:{
-    padding: 0
+  Nopadding: {
+    padding: 0,
   },
-  filterMenu:{
-    width:' 100%',
-    border: '1px lightgray solid',
+  filterMenu: {
+    width: " 100%",
+    border: "1px lightgray solid",
     display: "inline-flex",
-    borderRadius: 15
+    borderRadius: 15,
   },
-  
 });
 
 export default function ProductList(props) {
   const classes = useStyles();
-   const dispatch = useDispatch();
-   const productsList = useSelector(state => state.product.products)
+  const dispatch = useDispatch();
+  const productsList = useSelector((state) => state.product.products);
 
-   useEffect(() => {
-    const {cat_id} = props.category
+  useEffect(() => {
+    const { cat_id } = props.category;
     const payload = {
       params: {
         cat_id,
       },
-    }
+    };
 
-    dispatch(getProductsByCategory(payload))
-    },[])
-        
-    const handleSortASD = async() => {
-      // try{
-      //   const data = await axios.get('/getproductsByCategoryASD',{params:{category: props.category.cat_id}})
-        //let p = data.data.products;
-        //setProductsList(p)
-      // }catch(err){        console.error(err)
-      // }
+    dispatch(getProductsByCategory(payload));
+  }, []);
 
-    }
-    const handleSortDSD = async() => {
+  const handleSortASD = async () => {
+    // try{
+    //   const data = await axios.get('/getproductsByCategoryASD',{params:{category: props.category.cat_id}})
+    //let p = data.data.products;
+    //setProductsList(p)
+    // }catch(err){        console.error(err)
+    // }
+  };
+  const handleSortDSD = async () => {
     //try{
-      //const data = await axios.get('/getproductsByCategoryDSD',{params:{category: props.category.cat_id}})
-      //let p = data.data.products;
-     // setProductsList(p)
+    //const data = await axios.get('/getproductsByCategoryDSD',{params:{category: props.category.cat_id}})
+    //let p = data.data.products;
+    // setProductsList(p)
     //}catch(err){        console.error(err)
     ////}
-  }
+  };
 
   return (
     <Grid container spacing={1}>
       <Grid item xs={4} sm={8}>
         <Grid>
-        <List component="nav" className={classes.filterMenu}>
-        <ListItem button onClick={handleSortDSD}>
-          <ListItemIcon>
-            <ArrowUpwardIcon />
-          </ListItemIcon>
-          <ListItemText primary="گران ترین" />
-        </ListItem>
-        <ListItem button  onClick={handleSortASD}>
-          <ListItemIcon>
-            <ArrowDownwardIcon />
-          </ListItemIcon>
-          <ListItemText primary="ارزان ترین" />
-        </ListItem>
-      </List>
+          <List component="nav" className={classes.filterMenu}>
+            <ListItem button onClick={handleSortDSD}>
+              <ListItemIcon>
+                <ArrowUpwardIcon />
+              </ListItemIcon>
+              <ListItemText primary="گران ترین" />
+            </ListItem>
+            <ListItem button onClick={handleSortASD}>
+              <ListItemIcon>
+                <ArrowDownwardIcon />
+              </ListItemIcon>
+              <ListItemText primary="ارزان ترین" />
+            </ListItem>
+          </List>
         </Grid>
         <Grid container spacing={1}>
           <Grid item xs={4} sm={4} className={classes.displayList}>
-            { productsList && productsList.map((p) => 
-              (  <li key={p._id} className={classes.listStyle}>
-                   <ProductCard key={p._id} data={p} />
-                 </li>
-                )
-              )
-              }
-          </Grid>          
+            {productsList &&
+              productsList.map((p) => (
+                <li key={p._id} className={classes.listStyle}>
+                  <ProductCard key={p._id} data={p} />
+                </li>
+              ))}
+          </Grid>
         </Grid>
       </Grid>
       <Grid className={classes.Nopadding} item sm={4}>
-        <Basket />
+        {/* <Basket /> */}
         {/* <Order /> */}
       </Grid>
     </Grid>
