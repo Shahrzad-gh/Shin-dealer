265fffe96e570cf070c55753c198aec8d9e18286
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-14 17:50:58 +0330

    test

--- client/src/HOC/privateRoutes.js
+++ client/src/HOC/privateRoutes.js
@@ -1,23 +1,29 @@
-import React, { useContext } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React, { useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
 //import { AuthContext } from '../context/AuthContext';
-import AuthContext from '../context/AuthContext';
+import AuthContext from "../context/AuthContext";
 
 const PrivateRoutes = ({ component, roles, ...rest }) => {
   const { isLoggedIn, user } = useContext(AuthContext);
 
   return (
-    <Route {...rest} render={props => {
-
-      if (roles.includes(user.role)) {
-        return <Redirect to={{ pathname: '/Admin', state:{ from: props.location} }} />
-      }
-
-      if(!isLoggedIn){
-         return <Redirect  to={{ pathname: '/signin' }} />
+    <Route
+      {...rest}
+      render={(props) => {
+        if (roles.includes(user.role)) {
+          return (
+            <Redirect
+              to={{ pathname: "/Admin", state: { from: props.location } }}
+            />
+          );
         }
-    }} />
-  )
-}
+
+        if (!isLoggedIn) {
+          return <Redirect to={{ pathname: "/signin" }} />;
+        }
+      }}
+    />
+  );
+};
 
 export default PrivateRoutes;
