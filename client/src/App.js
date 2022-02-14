c2a8ff0732319932148cafd4f3c47942cc349505
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-14 17:50:38 +0330

    theme test

--- client/src/App.js
+++ client/src/App.js
@@ -25,38 +25,48 @@ import AllOrders from "./components/Admin/AllOrders";
 import PaymentPanel from "./components/PaymentPanel";
 import ManageOrder from "./components/Admin/ManageOrder";
 import Profile from "./components/User/Profile";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { orange } from "@mui/material/colors";
 
 axios.defaults.withCredentials = true;
 
 function App() {
+  const theme = createTheme({
+    status: {
+      danger: orange[500],
+    },
+  });
+
   return (
-    <AuthContextProvider>
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/SignIn" component={SignIn} />
-          <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="/ForgetPass" component={ForgetPass} />
-          <Route exact path="/Best" component={Best} />
-          <Route exact path="/Kids" component={Kids} />
-          <Route exact path="/Men" component={Men} />
-          <Route exact path="/Women" component={Women} />
-          <Route exact path="/ProductList" component={ProductList} />
-          <Route exact path="/Cart" component={Cart} />
-          <Route path="/Admin" roles={["admin"]} component={Dashboard} />
-          <Route exact path="/Product/:id" component={ProductDetails} />
-          <Route exact path="/Basket" component={Basket} />
-          <Route exact path="/WomenCloths" component={WomenCloths} />
-          <Route exact path="/MenCloths" component={MenCloths} />
-          <Route exact path="/Order/:id" component={Order} />
-          <Route exact path="/allOrders" component={AllOrders} />
-          <Route exact path="/PaymentPanel" component={PaymentPanel} />
-          <Route exact path="/ManageOrder/:id" component={ManageOrder} />
-          <Route exact path="/Profile" component={Profile} />
-        </div>
-      </Router>
-    </AuthContextProvider>
+    <ThemeProvider theme={theme}>
+      <AuthContextProvider>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/SignIn" component={SignIn} />
+            <Route exact path="/SignUp" component={SignUp} />
+            <Route exact path="/ForgetPass" component={ForgetPass} />
+            <Route exact path="/Best" component={Best} />
+            <Route exact path="/Kids" component={Kids} />
+            <Route exact path="/Men" component={Men} />
+            <Route exact path="/Women" component={Women} />
+            <Route exact path="/ProductList" component={ProductList} />
+            <Route exact path="/Cart" component={Cart} />
+            <Route path="/Admin" roles={["admin"]} component={Dashboard} />
+            <Route exact path="/Product/:id" component={ProductDetails} />
+            <Route exact path="/Basket" component={Basket} />
+            <Route exact path="/WomenCloths" component={WomenCloths} />
+            <Route exact path="/MenCloths" component={MenCloths} />
+            <Route exact path="/Order/:id" component={Order} />
+            <Route exact path="/allOrders" component={AllOrders} />
+            <Route exact path="/PaymentPanel" component={PaymentPanel} />
+            <Route exact path="/ManageOrder/:id" component={ManageOrder} />
+            <Route exact path="/Profile" component={Profile} />
+          </div>
+        </Router>
+      </AuthContextProvider>
+    </ThemeProvider>
   );
 }
 
