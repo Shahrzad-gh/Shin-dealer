fad02c74ae8ef448cae2b75bc1b7e54dfb31a809
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-15 18:26:15 +0330

    footer style update

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -3,15 +3,38 @@ import { makeStyles } from "@mui/styles";
 import AppBar from "@mui/material/AppBar";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-
-const useStyles = makeStyles((theme) => ({
+import Divider from "@mui/material/Divider";
+import eco from "../../../assets/eco.png";
+import paypal from "../../../assets/paypal.png";
+import visa from "../../../assets/visa.png";
+import mastercard from "../../../assets/mastercard.png";
+import sec from "../../../assets/sec.png";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
   },
   styledCard: {
     padding: 50,
   },
-  typography: { fontFamily: "Almarai", fontSize: "1rem" },
+  typography: { fontFamily: "Almarai !important", fontSize: "1rem !important" },
+  subscribe: {
+    width: "10vw",
+    margin: 20,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#717fe0",
+    color: "#fff",
+    border: "none",
+    fontFamily: "Almarai !important",
+    fontSize: "1rem !important",
+  },
+  copyright: {
+    fontSize: "0.8rem",
+    fontWeight: 100,
+  },
 }));
 
 export default function Footer() {
@@ -24,31 +47,80 @@ export default function Footer() {
           <Grid container>
             <Grid
               item
-              xs={4}
+              xs={6}
+              md={3}
               className={(classes.styledCard, classes.typography)}
             >
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-              استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-            </Grid>
+              <h4>خبرنامه</h4>
+              <p>info@shin-dealer.com</p>
+              <Divider />
 
+              <button className={classes.subscribe}>اشتراک</button>
+            </Grid>
             <Grid
               item
-              xs={4}
-              className={(classes.styledCard, classes.typography)}
-            ></Grid>
-            <Grid
-              item
-              xs={4}
+              xs={6}
+              md={3}
               className={(classes.styledCard, classes.typography)}
             >
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-              استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله
-              در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد
-              نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-              کتابهای زیادی در شصت و سه درصد گذشته حال و آینده
+              <h4>راه های ارتباطی</h4>
+              <p>شبکه های اجتماعی ما را دنبال کنید</p>
+              <FacebookOutlinedIcon />
+              <PinterestIcon />
+              <InstagramIcon />
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              md={3}
+              className={(classes.styledCard, classes.typography)}
+            >
+              <h4>راهنما</h4>
+              <ul>
+                <li>پیگیری سفارش</li>
+                <li>عودت</li>
+                <li>حمل و نقل</li>
+                <li>سوالات متداول</li>
+              </ul>
+            </Grid>
+            <Grid
+              item
+              xs={6}
+              md={3}
+              className={(classes.styledCard, classes.typography)}
+            >
+              <h4>دسته بندی ها</h4>
+              <ul>
+                <li>زنانه</li>
+                <li>مردانه</li>
+                <li>بچه گانه</li>
+              </ul>
             </Grid>
           </Grid>
         </Container>
+        <div className={classes.copyright}>
+          <div className="flex-c-m flex-w p-b-18">
+            <a href="#" className="m-all-1">
+              <img src={paypal} alt="ICON-PAY" />
+            </a>
+            <a href="#" className="m-all-1">
+              <img src={visa} alt="ICON-PAY" />
+            </a>
+            <a href="#" className="m-all-1">
+              <img src={mastercard} alt="ICON-PAY" />
+            </a>
+            <a href="#" className="m-all-1">
+              <img src={sec} alt="ICON-PAY" />
+            </a>
+            <a href="#" className="m-all-1">
+              <img src={eco} alt="ICON-PAY" />
+            </a>
+          </div>
+          <p>
+            Copyright ©2022 All rights reserved | This template is made with by
+            Shahrzad-gh
+          </p>
+        </div>
       </AppBar>
     </div>
   );
