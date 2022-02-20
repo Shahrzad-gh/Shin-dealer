42b8a1c6a71dc9659828233d5bf6e898e2a51ab2
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-20 09:36:57 +0330

    get all producs - redux update

--- client/src/Store/Reducers/productReducer.js
+++ client/src/Store/Reducers/productReducer.js
@@ -4,37 +4,51 @@ const initState = {
   products: [],
   productDetails: {},
   loading: false,
-  error: null
+  error: null,
 };
 
 export default (state = initState, action) => {
   switch (action.type) {
     case productConstants.GET_PRODUCT_DETAILS_BY_ID_SUCCESS:
-      return state = {
+      return (state = {
         ...state,
         loading: false,
-        productDetails : {...state.productDetails, [action.payload.productDetails._id]:action.payload.productDetails}
-      }
+        productDetails: {
+          ...state.productDetails,
+          [action.payload.productDetails._id]: action.payload.productDetails,
+        },
+      });
     case productConstants.GET_PRODUCT_DETAILS_BY_ID_FAILURE:
-      return state = {
+      return (state = {
         ...state,
         loading: false,
-        error : action.payload.error
-    }
+        error: action.payload.error,
+      });
     case productConstants.GET_PRODUCTS_BY_CATEGORY_SUCCESS:
-      return state = {
+      return (state = {
         ...state,
         loading: false,
-        products: action.payload.products
-      }
+        products: action.payload.products,
+      });
     case productConstants.GET_PRODUCTS_BY_CATEGORY_FAILURE:
-      return state = {
+      return (state = {
         ...state,
         loading: false,
-        error: action.payload.error
-      }
+        error: action.payload.error,
+      });
+    case productConstants.GET_ALL_PRODUCTS_SUCCESS:
+      return (state = {
+        ...state,
+        loading: false,
+        products: action.payload.products,
+      });
+    case productConstants.GET_ALL_PRODUCTS_FAILURE:
+      return (state = {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+      });
     default:
       return state;
-
   }
-}
+};
--- client/src/Store/actions/productActions.js
+++ client/src/Store/actions/productActions.js
@@ -1,40 +1,57 @@
-import { productConstants } from "./Types"
-import axios from 'axios';
+import { productConstants } from "./Types";
+import axios from "axios";
 
 export const getProductDetailsById = (payload) => {
   return async (dispatch) => {
-    const {id} = payload.params
+    const { id } = payload.params;
     let res;
     try {
-       res = await axios.get(`/getProductById/${id}`)
-        dispatch({
-          type: productConstants.GET_PRODUCT_DETAILS_BY_ID_SUCCESS,
-          payload: { productDetails: res.data.product }
-        })
+      res = await axios.get(`/getProductById/${id}`);
+      dispatch({
+        type: productConstants.GET_PRODUCT_DETAILS_BY_ID_SUCCESS,
+        payload: { productDetails: res.data.product },
+      });
     } catch (error) {
       dispatch({
         type: productConstants.GET_PRODUCT_DETAILS_BY_ID_FAILURE,
-        payload: { error: res.data.error }
-    });
-    }    
-  }
-}
+        payload: { error: res.data.error },
+      });
+    }
+  };
+};
 
 export const getProductsByCategory = (payload) => {
   return async (dispatch) => {
-    const {cat_id} = payload.params
+    const { cat_id } = payload.params;
     let res;
     try {
-       res = await axios.get(`/getProductsByCategory/${cat_id}`)
-        dispatch({
-          type: productConstants.GET_PRODUCTS_BY_CATEGORY_SUCCESS,
-          payload: { products: res.data.products }
-        })
+      res = await axios.get(`/getProductsByCategory/${cat_id}`);
+      dispatch({
+        type: productConstants.GET_PRODUCTS_BY_CATEGORY_SUCCESS,
+        payload: { products: res.data.products },
+      });
     } catch (error) {
       dispatch({
         type: productConstants.GET_PRODUCTS_BY_CATEGORY_FAILURE,
-        payload: { error: res.data.error }
-    });
-    }    
-  }
-}
+        payload: { error: res.data.error },
+      });
+    }
+  };
+};
+export const getAllProducts = () => {
+  return async (dispatch) => {
+    let res;
+    try {
+      res = await axios.get(`/getallproducts`);
+      dispatch({
+        type: productConstants.GET_ALL_PRODUCTS_SUCCESS,
+        payload: { products: res.data.products },
+      });
+    } catch (error) {
+      dispatch({
+        type: productConstants.GET_ALL_PRODUCTS_FAILURE,
+        payload: { error: res.data.error },
+      });
+    }
+  };
+};
