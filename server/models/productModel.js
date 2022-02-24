933c4984eabbf99df66beafd62eff8e3892c8b0e
Shahr Zad <gh.shahrzad@gmail.com>
2022-02-24 13:31:19 +0330

    update product model

--- server/models/productModel.js
+++ server/models/productModel.js
@@ -1,35 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
- name:{
+  name: {
     type: String,
-    required: [true, 'Please enter your name'],
+    required: [true, "Please enter your name"],
   },
   picture: {
-      img:{type: String},
-      id:{type: String}
-    },
-  count:{
-    type: String,
-    required: [true, 'Please enter count'],
+    img: { type: String },
+    id: { type: String },
   },
-  price:{
+  count: {
+    type: String,
+    required: [true, "Please enter count"],
+  },
+  price: {
     type: Number,
-    required: [true, 'Please enter price'],
+    required: [true, "Please enter price"],
+  },
+  information: {
+    Weight: { type: String },
+    Dimension: { type: String },
+    Material: { type: String },
+    Size: {
+      enum: ["S", "M", "L", "XL"],
+      default: "M",
+    },
   },
   description: {
     type: String,
   },
-  offer:{type:Number},
-  reviews:[
+  offer: { type: Number },
+  reviews: [
     {
-      userId:{type: mongoose.Schema.Types.ObjectId, ref:'User'},
-      userName: {Type: String},
-      review:{Type: String}
-    }
-],
-  category:{type: mongoose.Schema.Types.ObjectId, ref:'Category', required: true}
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      userName: { Type: String },
+      review: { Type: String },
+    },
+  ],
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: true,
+  },
 });
-const Product = mongoose.model('product', productSchema);
+const Product = mongoose.model("product", productSchema);
 
 module.exports = Product;
