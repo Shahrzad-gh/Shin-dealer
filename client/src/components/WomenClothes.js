4c058419133494043c15297ca4427308acbc0598
Shahr Zad <gh.shahrzad@gmail.com>
2022-04-15 17:25:02 +0430

    add basket - test

--- client/src/components/WomenClothes.js
+++ client/src/components/WomenClothes.js
@@ -2,9 +2,12 @@ import React from "react";
 import { useLocation } from "react-router";
 import ProductList from "./ProductList";
 import { makeStyles } from "@mui/styles";
+import Basket from "./Basket";
 
 const useStyles = makeStyles({
   container: {
+    display: "flex",
+    flexDirection: "row",
     padding: 20,
   },
 });
@@ -16,6 +19,7 @@ export default function WomenClothes() {
   return (
     <div className={classes.container}>
       <ProductList category={location.state} />
+      <Basket />
     </div>
   );
 }
