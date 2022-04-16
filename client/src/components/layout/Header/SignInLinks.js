83fa8c0754a902386dccabd24c6eba284405b22f
Shahr Zad <gh.shahrzad@gmail.com>
2022-04-16 19:49:20 +0430

    set items center

--- client/src/components/layout/Header/SignInLinks.js
+++ client/src/components/layout/Header/SignInLinks.js
@@ -17,6 +17,7 @@ const useStyles = makeStyles((theme) => ({
     display: "none",
     [theme.breakpoints.up("md")]: {
       display: "flex",
+      alignItems: "center"
     },
   },
   sectionMobile: {
