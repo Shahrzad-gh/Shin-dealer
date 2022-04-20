fd1f5d6cc407cef4ce4c80f64868a98a1f4c0ab5
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2022-04-20 12:02:59 +0430

    update

--- client/src/components/Admin/Dashboard.js
+++ client/src/components/Admin/Dashboard.js
@@ -122,7 +122,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Dashboard() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   const handleDrawerOpen = () => {
     setOpen(true);
   };
